019a0d645fe878cc493b7cd4df6cb8f6cc037db6
LIU <66453914+CLOT-LIU@users.noreply.github.com>
2021-08-23 16:25:07 +0800

    Update https.js

--- https.js
+++ https.js
@@ -1,12 +1,12 @@
 shadowsocks=cm-hk.stunnel.vip:4800, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=HKBGP-PRO
 shadowsocks=jp.stunnel.vip:6300, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=JP-BGP
-shadowsocks=uk.gotochinatown.net:10882, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=London
-shadowsocks=ru.gotochinatown.net:10883, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=Saint Petersburg
+shadowsocks=uk.gotochinatown.net:10882, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=伦敦[英国]
+shadowsocks=ru.gotochinatown.net:10883, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=圣彼得堡[俄罗斯]
 shadowsocks=cm-us.stunnel.vip:3300, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=US-BGP
 shadowsocks=us.gotochinatown.net:10886, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=US-CN2-01
 shadowsocks=us.gotochinatown.net:10887, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=US-CN2-04
-shadowsocks=tr.gotochinatown.net:10885, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=伊斯坦布尔
+shadowsocks=tr.gotochinatown.net:10885, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=伊斯坦布尔[土耳其]
 shadowsocks=tw.gotochinatown.net:10881, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=台湾[高QoS]
-shadowsocks=in.gotochinatown.net:10884, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=孟买
+shadowsocks=in.gotochinatown.net:10884, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=孟买[印度]
 shadowsocks=jpl.gotochinatown.net:10888, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=日本原生
-shadowsocks=de.gusa.xyz:443, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=纽伦堡
+shadowsocks=de.gusa.xyz:443, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=纽伦堡[德国]
