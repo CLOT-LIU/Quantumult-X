86cc899f3ab29ad9297beb195f1f44045174408e
LIU <66453914+CLOT-LIU@users.noreply.github.com>
2021-08-23 15:18:56 +0800

    Create shadowsocks.js

---
+++ shadowsocks.js
@@ -0,0 +1,3 @@
+shadowsocks=cm-hk.stunnel.vip:4800, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=HKBGP-PRO
+shadowsocks=jp.stunnel.vip:6300, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=JP-BGP
+shadowsocks=uk.gotochinatown.net:10882, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=London
