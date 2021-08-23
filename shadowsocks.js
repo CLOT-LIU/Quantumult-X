291463b77416a084a2c48cd25a906400d7fb90d1
LIU <66453914+CLOT-LIU@users.noreply.github.com>
2021-08-23 15:29:06 +0800

    Update shadowsocks.js

--- shadowsocks.js
+++ shadowsocks.js
@@ -1,3 +1,14 @@
 shadowsocks=cm-hk.stunnel.vip:4800, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=HKBGP-PRO
 shadowsocks=jp.stunnel.vip:6300, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=JP-BGP
 shadowsocks=uk.gotochinatown.net:10882, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=London
+shadowsocks=ru.gotochinatown.net:10883, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=Saint Petersburg
+shadowsocks=cm-us.stunnel.vip:3300, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=US-BGP
+shadowsocks=us.gotochinatown.net:10886, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=US-CN2-01
+shadowsocks=us.gotochinatown.net:10887, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=US-CN2-04
+shadowsocks=tr.gotochinatown.net:10885, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=伊斯坦布尔
+shadowsocks=tw.gotochinatown.net:10881, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=台湾[高QoS]
+shadowsocks=in.gotochinatown.net:10884, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=孟买
+shadowsocks=jpl.gotochinatown.net:10888, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=日本原生
+shadowsocks=de.gusa.xyz:443, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=纽伦堡
+shadowsocks=cm.stunnel.vip:4700, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=香港HKBN
+shadowsocks=cm.stunnel.vip:4700, method=none, password=0aa4b81079b9a6c3ab9cfdc3f7c8a63f, fast-open=false, udp-relay=false, tag=香港HKT
